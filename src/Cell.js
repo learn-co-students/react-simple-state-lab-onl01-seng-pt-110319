084d7def89ab656369b3bf07523cb8db8f0d97f3
github username <github email address>
2020-09-01 16:41:39 -0400

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 
 export default class Cell extends Component{
-    constructor (){
-        super()
+    constructor (props){
+        super(props)
         this.state={
             color: this.props.value
         }
